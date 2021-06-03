cf15ab1331e70606778b88be6963651fc43bf44b
jmart6784 <jmartinez6784@yahoo.com>
2021-06-03 12:29:07 -0700

    renamed users controller and added show view for users

--- app/controllers/user_controller.rb
+++
@@ -1,7 +0,0 @@
-class UserController < ApplicationController
-  def index
-    # sql = "Select * from ... your sql query here"
-    # records_array = ActiveRecord::Base.connection.execute(sql)
-    @users = User.all
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "static#index"
-  get "/users", to: "user#index"
+  resources :users, only: [:index, :show]
 end
