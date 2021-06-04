2371b5b1fcaac83bd247bb6b2679b974a07916f8
jmart6784 <jmartinez6784@yahoo.com>
2021-06-03 21:03:32 -0700

    Added friend request ajax logic

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,2 +1,19 @@
 class FriendRequestsController < ApplicationController
+  def ajax_friend_request
+    @requestor = User.find(params[:requestor_id])
+    @receiver = User.find(params[:receiver_id])
+
+    @friend_request = FriendRequest.find_by(requestor_id: @requestor.id, receiver_id: @receiver.id)
+
+    unless @requestor === @receiver || !@friend_request.nil?
+      @request_text = "Sent"
+      FriendRequest.create(requestor_id: @requestor.id, receiver_id: @receiver.id);
+    else
+      @request_text = "Add"
+    end
+
+    respond_to do |format|
+      format.js {}
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "users#index"
   resources :users, only: [:index, :show]
+
+  resources :friend_requests, only: [:update] do
+    member do
+      put :ajax_friend_request
+    end
+  end
 end
