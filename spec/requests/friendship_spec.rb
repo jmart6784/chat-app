cbca73a9ccbb599d62ecac3786742ccb019484c1
jmart6784 <jmartinez6784@yahoo.com>
2021-06-14 22:54:18 -0700

    Added test to get friends list page if authenticated

---
+++ spec/requests/friendship_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "Friendships", type: :request do
+  user_1 = User.find_by(email: "rspec@example.com")
+  user_2 = User.find_by(email: "rspec2@example.com")
+
+  if user_1.nil?
+    user_1 = User.create(
+      email: "rspec@example.com", 
+      password: "password", 
+      password_confirmation: "password"
+    )
+  end
+
+  if user_2.nil?
+    user_2 = User.create(
+      email: "rspec2@example.com", 
+      password: "password", 
+      password_confirmation: "password"
+    )
+  end
+
+  describe "GET /index" do
+    it "gets friend list if authenticated" do
+      sign_out(user_1)
+      sign_in(user_1)
+      get friends_path(id: user_1.id)
+      expect(response).to render_template(:friends) 
+    end
+  end
+end
