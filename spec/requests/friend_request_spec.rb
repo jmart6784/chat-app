2c05c205594a547987468edacebc82f879e48f65
jmart6784 <jmartinez6784@yahoo.com>
2021-06-14 21:26:21 -0700

    Added test to avoid sending a friend request if one already exists

--- spec/requests/friend_request_spec.rb
+++ spec/requests/friend_request_spec.rb
@@ -20,23 +20,21 @@ RSpec.describe "FriendRequests", type: :request do
     )
   end
 
-  friend_request = FriendRequest.create(
-    requestor_id: user_1.id,
-    receiver_id: user_2.id
-  )
+  let(:valid_params) do
+    params = {
+      id: 0,  
+      requestor_id: user_1.id, 
+      receiver_id: user_2.id, 
+      html_id: "request-#{user_2.id}", 
+      format: :js
+    }
+  end
 
   describe "PUT /ajax_friend_request" do
     it "Creates friend request given two users" do
-      params = {
-        id: 0,  
-        requestor_id: user_1.id, 
-        receiver_id: user_2.id, 
-        html_id: "request-#{user_2.id}", 
-        format: :js
-      }
       sign_out(user_1)
       sign_in(user_1)
-      put ajax_friend_request_friend_request_path(params)
+      put ajax_friend_request_friend_request_path(valid_params)
       fr = FriendRequest.find_by(requestor_id: user_1.id, receiver_id: user_2.id)
       expect(fr.nil?).to eql(false)
     end
@@ -54,6 +52,15 @@ RSpec.describe "FriendRequests", type: :request do
       put ajax_friend_request_friend_request_path(params)
       fr = FriendRequest.find_by(requestor_id: user_1.id, receiver_id: user_1.id)
       expect(fr.nil?).to eql(true)
+    end
+
+    it "Doesn't create another friend request if one already exists" do
+      sign_out(user_1)
+      sign_in(user_1)
+      before_count = FriendRequest.all.count
+      put ajax_friend_request_friend_request_path(valid_params)
+      after_count = FriendRequest.all.count
+      expect(before_count).to eql(after_count)
     end
   end
 end
