1cf8ec164a7a53f21d265b62e0a6190460b0dda2
jmart6784 <jmartinez6784@yahoo.com>
2021-06-18 23:40:48 -0700

    Added Chat model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_17_054125) do
+ActiveRecord::Schema.define(version: 2021_06_19_063855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chats", force: :cascade do |t|
+    t.text "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "friend_requests", force: :cascade do |t|
     t.integer "requestor_id"
