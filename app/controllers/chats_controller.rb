33069d64c3a98841cc54bc001af687ff1a0a2f54
jmart6784 <jmartinez6784@yahoo.com>
2021-06-21 13:24:10 -0700

    Add new and create action to chat controllers along with form in the view. also added host_id to chat model

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -8,10 +8,28 @@ class ChatsController < ApplicationController
   end
 
   def new
-    
+    @chat = Chat.new
+  end
+
+  def create
+    @chat = Chat.new(chat_params)
+    @chat.host_id = current_user.id
+
+    if @chat.save
+      redirect_to @chat
+    else 
+      flash.now[:danger] = "Can't create this Chat room, there are errors."
+      render "new"
+    end
   end
 
   def ajax_create_chat
 
+  end
+
+  private
+
+  def chat_params
+    params.require(:chat).permit(:name)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_19_063855) do
+ActiveRecord::Schema.define(version: 2021_06_21_195116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_06_19_063855) do
     t.text "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "host_id"
   end
 
   create_table "friend_requests", force: :cascade do |t|
