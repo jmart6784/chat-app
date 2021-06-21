8dc6310c29b6e15f456f0d6419e1ce658c9d4bf3
jmart6784 <jmartinez6784@yahoo.com>
2021-06-21 15:00:08 -0700

    Fixed chat user associations

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,5 +1,5 @@
 class Chat < ApplicationRecord
-  belongs_to :user, class_name: :User, foreign_key: :id
+  belongs_to :host, class_name: :User, foreign_key: :host_id, optional: true
 
   validates :name, presence: true, length: { minimum: 1, maximum: 60 }
   validates :host_id, presence: true
