799d171509247e12259d1f6c7e87d95bde2b1eb9
jmart6784 <jmartinez6784@yahoo.com>
2021-06-21 13:27:03 -0700

    Added model assciations for user and chat

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,2 +1,6 @@
 class Chat < ApplicationRecord
+  belongs_to :user, class_name: :User, foreign_key: :id
+
+  validates :name, presence: true, length: { minimum: 1, maximum: 60 }
+  validates :host_id, presence: true
 end
