2375ccb5ad941932de869a146ca9d9b56b2e305c
jmart6784 <jmartinez6784@yahoo.com>
2021-06-22 21:02:56 -0700

    added create action and message form in view

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -7,6 +7,7 @@ class ChatsController < ApplicationController
 
   def show
     @chat = Chat.find(params[:id])
+    @message = Message.new
   end
 
   def new
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,10 @@
 class MessagesController < ApplicationController
   def create
-    
+    return unless message_params[:user_id].to_i === current_user.id
+    @chat = Chat.find(message_params[:chat_id])
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to @chat
   end
 
   def edit
@@ -13,5 +17,11 @@ class MessagesController < ApplicationController
 
   def destroy
     
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :chat_id, :user_id)
   end
 end
