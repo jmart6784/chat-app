8d090072bd471b7f1934ff15e0ab4327d1228cfd
jmart6784 <jmartinez6784@yahoo.com>
2021-06-25 23:10:32 -0700

    Edited invite associatons and invitations view

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,6 +1,7 @@
 class Invite < ApplicationRecord
   belongs_to :host, class_name: :User
   belongs_to :guest, class_name: :User
+  belongs_to :chat
 
   validates :host_id, presence: true
   validates :guest_id, presence: true
