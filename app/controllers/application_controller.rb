192ee5c801814e2c3a59f19940ea122a53486970
jmart6784 <jmartinez6784@yahoo.com>
2021-06-25 22:30:19 -0700

    Implemented ajax invite

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  helper_method :friend_list, :is_friend?, :request_pending?, :response_pending?, :full_name
+  helper_method :friend_list, :is_friend?, :request_pending?, :response_pending?, :full_name, :invite_exists?
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -28,6 +28,14 @@ class ApplicationController < ActionController::Base
 
   def full_name(user)
     "#{user.first_name} #{user.last_name}"
+  end
+
+  def invite_exists?(host, guest, chat)
+    !Invite.find_by(
+      host_id: host.id, 
+      guest_id: guest.id, 
+      chat_id: chat.id
+    ).nil?
   end
 
   protected
--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -5,7 +5,25 @@ class InvitesController < ApplicationController
   end
 
   def ajax_invite_create
-    
+    @guest = User.find(params[:guest_id])
+    @chat = Chat.find(params[:chat_id])
+
+    existing_invite = Invite.find_by(
+      host_id: current_user.id, 
+      guest_id: @guest.id,
+      chat_id: @chat.id
+    )
+
+    @invite_created = false
+
+    if existing_invite.nil? && current_user.hosted_chats.include?(@chat) && params[:host_id].to_i === current_user.id
+      @invite_created = true
+      Invite.create(
+        host_id: current_user.id, 
+        guest_id: @guest.id,
+        chat_id: @chat.id
+      )
+    end
 
     respond_to do |format|
       format.js {}
