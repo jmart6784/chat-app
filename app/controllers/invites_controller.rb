1ae0e404969fbbaefccaad7d4f7887fef9135770
jmart6784 <jmartinez6784@yahoo.com>
2021-06-30 20:47:57 -0700

    Prevented user to invite friend if already in chat

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -15,7 +15,11 @@ class InvitesController < ApplicationController
 
     @invite_created = false
 
-    if !invite_exists?(current_user, @guest, @chat) && current_user.hosted_chats.include?(@chat) && params[:host_id].to_i === current_user.id && is_friend?(current_user, @guest)
+    if !invite_exists?(current_user, @guest, @chat) && 
+    current_user.hosted_chats.include?(@chat) && 
+    params[:host_id].to_i === current_user.id && 
+    is_friend?(current_user, @guest) && 
+    !chat_joined?(@guest, @chat)
       @invite_created = true
       Invite.create(
         host_id: current_user.id, 
