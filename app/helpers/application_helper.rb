6a26f9a71201b10dd5221a73a84735aec7f1088b
jmart6784 <jmartinez6784@yahoo.com>
2021-07-02 23:05:38 -0700

    styled nav bar and added google fonts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def user_avatar(user, size=40, id = "", class_name="")
     if user.avatar.attached?
-      image_tag(user.avatar.variant(resize: "#{size}x#{size}!"), id: id, class_name: class_name)
+      image_tag(user.avatar.variant(resize: "#{size}x#{size}!"), id: id, class: class_name)
     else
-      image_tag("default-profile.png", width: "#{size}", height: "#{size}", id: id, class_name: class_name)
+      image_tag("default-profile.png", width: "#{size}", height: "#{size}", id: id, class: class_name)
     end
   end
 end
