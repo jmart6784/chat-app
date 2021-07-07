01063dc7394f86dd4473c5cc276f0f108f2c8ef3
jmart6784 <jmartinez6784@yahoo.com>
2021-07-07 15:05:06 -0700

    Added javascript validations to new chat form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,3 +28,25 @@ document.addEventListener('turbolinks:load', () => {
     }
   });
 });
+
+// New chat field validations
+document.addEventListener('turbolinks:load', () => {
+  const nameInput = document.getElementById("chat-name-input");
+  const validationLabel = document.getElementById("new-chat-name-errors-length");
+  const charCount = document.getElementById("new-chat-current-char");
+
+  if (nameInput) {
+    nameInput.addEventListener("input", () => {
+      document.getElementById("new-chat-name-errors-length").style.display = "block";
+      charCount.textContent = nameInput.value.length
+
+      if (nameInput.value.length === 0 || nameInput.value.length > 30) {
+        validationLabel.style.color = "red";
+        charCount.style.color = "red";
+      } else {
+        validationLabel.style.color = "rgb(38, 207, 38)";
+        charCount.style.color = "rgb(38, 207, 38)";
+      }
+    }); 
+  }
+});
