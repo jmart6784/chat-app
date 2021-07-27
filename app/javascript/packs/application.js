f3a83e2b6dfd731dc4175495f44002198df0dad9
jmart6784 <jmartinez6784@yahoo.com>
2021-07-26 23:14:14 -0700

    Moved chat show javascript to seperate file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,14 @@ ActiveStorage.start();
 
 require("@fortawesome/fontawesome-free");
 
+import chatShow from "./chats/chatShow";
+import formValidations from "./general_forms/formValidations";
+
+document.addEventListener('turbolinks:load', () => {
+  chatShow();
+  formValidations();
+});
+
 document.addEventListener('turbolinks:load', () => {
   const navPic = document.getElementById("nav-pic");
   const dropContent = document.getElementById("nav-drop-content");
@@ -353,87 +361,5 @@ document.addEventListener("turbolinks:load", () => {
     setTimeout(() => {
       alert.style.display = "none";
     }, 4000);
-  }
-});
-
-// Chat show view hide/show guests
-document.addEventListener("turbolinks:load", () => {
-  const guestBtn = document.getElementById("chat-show-guests-btn");
-  const guestDiv = document.getElementById("chat-show-guests-div");
-  const closeBtn = document.getElementById("close-guest-div-btn");
-
-  if (guestBtn) {
-    guestDiv.style.display = "none";
-
-    guestBtn.addEventListener("click", () => {
-      if (guestDiv.style.display === "flex") {
-        guestDiv.style.display = "none";
-      } else if (guestDiv.style.display === "none") {
-        guestDiv.style.display = "flex";
-      }
-    });
-
-    closeBtn.addEventListener("click", () => {
-      guestDiv.style.display = "none";
-    });
-  }
-});
-
-// Chat show view hide/show options
-document.addEventListener("turbolinks:load", () => {
-  const optionsBtn = document.getElementById("chat-show-options-btn");
-  const optionsIcon = document.getElementById("options-dash");
-  const optionsDiv = document.getElementById("host-options");
-
-  if (optionsBtn) {
-    optionsDiv.style.display = "none";
-
-    const handleAppearance = () => {
-      if (optionsDiv.style.display === "flex") {
-        optionsDiv.style.display = "none";
-      } else if (optionsDiv.style.display === "none") {
-        optionsDiv.style.display = "flex";
-      }
-    };
-
-    optionsIcon.addEventListener("click",  handleAppearance);
-    optionsBtn.addEventListener("click", () => {
-      setTimeout(() => {
-        handleAppearance();
-      }, 100);
-    });
-
-    document.addEventListener("click", (e) => {
-      if (e.target != optionsBtn) {
-        optionsDiv.style.display = "none";
-      }
-    });
-  }
-});
-
-// Auto scroll to bottom of chat box
-document.addEventListener("turbolinks:load", () => {
-  const chatBox = document.getElementById("chat-box");
-  if (chatBox) {
-    chatBox.scrollTop = chatBox.scrollHeight;
-  }
-});
-
-// Chat message character counter
-document.addEventListener("turbolinks:load", () => {
-  const charCounter = document.getElementById("message-char-text");
-  const messageInput = document.getElementById("message-input-field");
-
-  if (messageInput) {
-    messageInput.addEventListener("input", () => {
-      if (messageInput.value.length <= 640) {
-        charCounter.style.color = "white";
-      } else {
-        charCounter.style.color = "red";
-        messageInput.value = messageInput.value.substring(0, messageInput.value.length - 1);
-      }
-
-      charCounter.textContent = `${messageInput.value.length}/640`;
-    });
   }
 });
---
+++ app/javascript/packs/chats/chatShow.js
@@ -0,0 +1,79 @@
+const chatShow = () => {
+
+  // Chat show view hide/show guests
+  const guestBtn = document.getElementById("chat-show-guests-btn");
+  const guestDiv = document.getElementById("chat-show-guests-div");
+  const closeBtn = document.getElementById("close-guest-div-btn");
+
+  if (guestBtn) {
+    guestDiv.style.display = "none";
+
+    guestBtn.addEventListener("click", () => {
+      if (guestDiv.style.display === "flex") {
+        guestDiv.style.display = "none";
+      } else if (guestDiv.style.display === "none") {
+        guestDiv.style.display = "flex";
+      }
+    });
+
+    closeBtn.addEventListener("click", () => {
+      guestDiv.style.display = "none";
+    });
+  }
+
+  // Auto scroll to bottom of chat box
+  const chatBox = document.getElementById("chat-box");
+  if (chatBox) {
+    chatBox.scrollTop = chatBox.scrollHeight;
+  }
+
+  // Chat message character counter
+  const charCounter = document.getElementById("message-char-text");
+  const messageInput = document.getElementById("message-input-field");
+
+  if (messageInput) {
+    messageInput.addEventListener("input", () => {
+      if (messageInput.value.length <= 640) {
+        charCounter.style.color = "white";
+      } else {
+        charCounter.style.color = "red";
+        messageInput.value = messageInput.value.substring(0, messageInput.value.length - 1);
+      }
+
+      charCounter.textContent = `${messageInput.value.length}/640`;
+    });
+  }
+
+  // Chat show view hide/show options
+  const optionsBtn = document.getElementById("chat-show-options-btn");
+  const optionsIcon = document.getElementById("options-dash");
+  const optionsDiv = document.getElementById("host-options");
+
+  if (optionsBtn) {
+    optionsDiv.style.display = "none";
+
+    const handleAppearance = () => {
+      if (optionsDiv.style.display === "flex") {
+        optionsDiv.style.display = "none";
+      } else if (optionsDiv.style.display === "none") {
+        optionsDiv.style.display = "flex";
+      }
+    };
+
+    optionsIcon.addEventListener("click",  handleAppearance);
+    optionsBtn.addEventListener("click", () => {
+      setTimeout(() => {
+        handleAppearance();
+      }, 100);
+    });
+
+    document.addEventListener("click", (e) => {
+      if (e.target != optionsBtn) {
+        optionsDiv.style.display = "none";
+      }
+    });
+  }
+
+}
+
+export default chatShow;
