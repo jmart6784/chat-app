905600f7d1978c4942882b903ec98049f90b28ea
jmart6784 <jmartinez6784@yahoo.com>
2021-07-30 23:42:05 -0700

    Implemented user search in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,25 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    if params[:search]
+      search_txt = params[:search].downcase
+      @users = []
+
+      search_words = search_txt.split(" ")
+      
+      User.all.select do |user|
+        next if current_user === user
+
+        search_words.each do |word|
+          if user.first_name.downcase[word] || user.last_name.downcase[word] || user.username.downcase[word]
+            @users << user
+          end
+        end
+      end
+
+      @users.uniq!
+    else
+      @users = User.all
+    end
   end
 
   def show
