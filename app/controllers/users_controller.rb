2aba01c11e5fa121adeb4fbe877b9b4a7859eed9
jmart6784 <jmartinez6784@yahoo.com>
2021-08-04 20:53:08 -0700

    Added pagination to users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,9 @@ class UsersController < ApplicationController
       end
 
       @users.uniq!
+      @users = @users.paginate(page: params[:page], per_page: 21)
     else
-      @users = User.all
+      @users = User.all.paginate(page: params[:page], per_page: 21)
     end
   end
 
