42edc149e249ded5d2496c12653cb27de2624cc9
jmart6784 <jmartinez6784@yahoo.com>
2021-08-04 21:02:34 -0700

    Made pagination controls responsive to mobile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,9 @@ class UsersController < ApplicationController
       end
 
       @users.uniq!
-      @users = @users.paginate(page: params[:page], per_page: 21)
+      @users = @users.paginate(page: params[:page], per_page: 1)
     else
-      @users = User.all.paginate(page: params[:page], per_page: 21)
+      @users = User.all.paginate(page: params[:page], per_page: 1)
     end
   end
 
