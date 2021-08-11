6fa5a76dde92ed52d62f611155e4da3e5f7a4985
jmart6784 <jmartinez6784@yahoo.com>
2021-08-10 22:08:48 -0700

    Configured actioncable CORS for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,8 +58,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://quick-chats-app.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://quick-chats-app.herokuapp.com', /http:\/\/quick-chats-app.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
